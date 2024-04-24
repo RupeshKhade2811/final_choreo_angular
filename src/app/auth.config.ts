bca8849aaf32b20c31e806466a3c411f09a8a73b
RupeshKhade2811 <125442219+RupeshKhade2811@users.noreply.github.com>
2024-04-24 19:06:12 +0530

    Update auth.config.ts

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,15 +1,15 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'https://api.asgardeo.io/t/orgenox1/oauth2/token',
-    clientId: 'cMiX7ul5LdNI8CNVEeNqFMcFpvEa',
+    issuer: 'https://api.asgardeo.io/t/massil2016/oauth2/token',
+    clientId: 'Efa4ijXGqcD9GbuA4Ol8AuDWpY4a',
     responseType: 'code',
-    redirectUri: 'https://0429ca28-f68d-4536-98e6-497f85bb47b1.e1-us-cdp-2.choreoapps.dev/dashboard',
+    redirectUri: 'https://e3baa385-6f21-45ff-9c5c-6cf38b4890de.e1-us-east-azure.choreoapps.dev/dashboard',
    //redirectUri:'http://localhost:4200/dashboard',
-    tokenEndpoint: 'https://api.asgardeo.io/t/orgenox1/oauth2/token',
+    tokenEndpoint: 'https://api.asgardeo.io/t/massil2016/oauth2/token',
     skipIssuerCheck: true,
-    logoutUrl: 'https://api.asgardeo.io/t/orgenox1/oidc/logout',
-    postLogoutRedirectUri: 'https://0429ca28-f68d-4536-98e6-497f85bb47b1.e1-us-cdp-2.choreoapps.dev/dashboard',
+    logoutUrl: 'https://api.asgardeo.io/t/massil2016/oidc/logout',
+    postLogoutRedirectUri: 'https://e3baa385-6f21-45ff-9c5c-6cf38b4890de.e1-us-east-azure.choreoapps.dev/dashboard',
     scope: 'openid profile internal_user_mgt_view', 
     useSilentRefresh: false,
     silentRefreshTimeout: 50000000,
